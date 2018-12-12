537f97bfcd4648178c9545b2a73b1977e424e2a5
Yuda Cogati <yudapc@gmail.com>
2018-12-12 17:42:54 +0700

    allow method options

--- server.js
+++ server.js
@@ -16,6 +16,19 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.options("/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,PATCH,POST,DELETE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  res.sendStatus(200);
+});
+
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
